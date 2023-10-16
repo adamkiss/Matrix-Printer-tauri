d74241a25349f6ea604ce53543429df9e4cf0d68
Adam Kiss <iam@adamkiss.com>
2023-10-16 18:27:22 +0200

    add and configure tauri

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    // prevent obscuring Rust errors
+    clearScreen: false,
+    // Fixed port for tauri
+    server: {
+        port: 6969,
+        strictPort: true,
+    },
+    // Other Tauri stuff
+    envPrefix: ["VITE_", "TAURI_"],
+    build: {
+        target: ["es2021", "chrome100", "safari14"],
+        minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+        sourcemap: !!process.env.TAURI_DEBUG,
+    }
+})
